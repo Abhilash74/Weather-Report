ae97482ae9de5b9807edeb915c4933fc8f7d4171
Abhilash74 <abhilash.c@nineleaps.com>
2022-08-03 23:26:27 +0530

    Merge branch 'main' of https://github.com/Abhi110895/Weather-Report into main

--- weather-report/src/app/services/city-service.service.ts
+++ weather-report/src/app/services/city-service.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityServiceService {
 
-  constructor() { }
+  public appId : string = 'd4594364698122bfd1c4b3eb5f2ff19f';
+
+  constructor(private http: HttpClient) { }
+
+  getCity(city: string): any {
+  const url: string = 'http://api.openweathermap.org/geo/1.0/direct';
+    const params = new HttpParams()
+   .set('q', city)
+   .set('limit', 1).set('appid',this.appId);
+  return this.http.get(url,{params:params});
+  }
+
+  getCityWeatherDetails(data: any){
+   const url = 'https://api.openweathermap.org/data/2.5/weather';
+    const params = new HttpParams().set('lat',data.lat).set('lon',data.lon).set('appid',this.appId);
+    return this.http.get(url,{params:params});
+  }
 }
